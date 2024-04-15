406330134f1d3a8b1a9dde34a11ef52378d17cfc
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-04-14 21:18:37 -0500

    Commit introduction

---
+++ src/Introduction/HelloWorld.java
@@ -0,0 +1,17 @@
+package Introduction;
+
+public class HelloWorld {
+    // Copie and paste the code below
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// solved exercise:
+
+//        class Main {
+//            public static void main(String[] args) {
+//                System.out.println("Hello World!");
+//            }
+//        }
+
---
+++ src/Introduction/TheMainClass.java
@@ -0,0 +1,16 @@
+package Introduction;
+
+public class TheMainClass {
+        // Copie and paste the code below
+        public static void main(String[] args) {
+            System.out.print("I'm learning Java with Coddy");
+        }
+    }
+
+//  solved exercise:
+
+//        class Main {
+//            public static void main(String[] args) {
+//                System.out.print("I'm learning Java with Coddy");
+//            }
+//        }
